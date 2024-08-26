72a8b687fe9212389774ab90e9b3b7146da9d825
meoDzz <minhnhatledinh.vn@gmail.com>
2024-08-27 00:46:48 +0900

    update switch language

--- src/changeLanguage.js
+++ src/changeLanguage.js
@@ -4,28 +4,20 @@ document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('.languageSection a');
     const data = {
         "english": {
-            "title-header": "English Title 1",
-            "description-header": "This is the first English description.",
-            "title-bottom": "English Title 2",
-            "description-bottom": "This is the second English description.",
-            "title-last": "English Title 3",
-            "description-last": "This is the third English description.",
+            "title-aboutus": "About Us",
+            "description-aboutus": "Our Laboratory aims to answer emerging needs of the society by proposals of novel robotic systems, ranging from fabrication of sensors, actuators, and intelligence. We particulaly focus on dynamic investigation of nature phenomena to find out the underlying, dominating mechanisms that could then give hints to fabrication of novel structures. Ideas are shaped under the light of novel technologies with integration of scientific methods, then integrated in robotic systems with specific applications. Results obtained in this process could be inversely utilized to understand the natures.",
             "title-sub": "English Title 4",
-            "description-sub": "This is the fourth English description.",
+                "description-sub": "This is the fourth English description.",
             "title-txt": "English Title 5",
-            "description-txt": "This is the fifth English description."
+                "description-txt": "This is the fifth English description."
         },
         "japanese": {
-            "title-header": "日本語のタイトル1",
-            "description-header": "これは最初の日本語の説明です。",
-            "title-bottom": "日本語のタイトル2",
-            "description-bottom": "これは2番目の日本語の説明です。",
-            "title-last": "日本語のタイトル3",
-            "description-last": "これは3番目の日本語の説明です。",
+            "title-aboutus": "About Us",
+            "description-aboutus": "自然界のすべての現象には、何らかの形で必ずダイナミクスが関与している。このダイナミクスを理解できれば、その現象を生じさせるために、メカニズムがどのように進化してきたかを理解することが可能になります。また、そのメカニズムを ロボットの駆動装置または感覚装置に応用することで、新しい 機構を創出できると考えられる。",
             "title-sub": "日本語のタイトル4",
-            "description-sub": "これは4番目の日本語の説明です。",
+                "description-sub": "これは4番目の日本語の説明です。",
             "title-txt": "日本語のタイトル5",
-            "description-txt": "これは5番目の日本語の説明です。"
+                "description-txt": "これは5番目の日本語の説明です。"
         }
     };
     buttons.forEach(button => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "color-primary": "#01051e",
+        "color-primary": "#f5f5f5",
+        "color-highlight": "#e05d2d",
         "color-primary-light": "#020726",
         "color-primary-dark": "#010417",
         "text-color-secondary": "#ff7d3b",
         "color-gray": "#333",
-        "color-white": "#fff",
+        "color-white": "#000",
         "color-blob": "#A427DF",
       }
     },
