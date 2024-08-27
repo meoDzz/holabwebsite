5e17dbbbbf43625293626944ef0497d7e9a9248a
meoDzz <minhnhatledinh.vn@gmail.com>
2024-08-28 00:48:44 +0900

    gallery

--- src/menuLoader.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('menu.html')
-        .then(response => response.text())
-        .then(data => {
-            document.body.insertAdjacentHTML('afterbegin', data);
-        })
-        .catch(error => console.error('Error loading the menu:', error));
-});
